0fe25ee7e63dd7064fbf55a533e6a8e52d478181
henrytrianta <henry.trianta@gmail.com>
2021-05-27 20:22:43 +0800

    test: adding postgre config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,18 +1,15 @@
-const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
-
 module.exports = ({ env }) => ({
   defaultConnection: "default",
   connections: {
     default: {
       connector: "bookshelf",
       settings: {
-        client: "postgres",
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
+        client: process.env.DATABASE_CLIENT,
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
+        database: process.env.DATABASE_NAME,
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
       },
       options: {
         ssl: false,
